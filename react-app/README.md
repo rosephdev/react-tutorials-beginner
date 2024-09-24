be9ec0ccdd14057b56688581ccded05fe7d0ff9f
rdtungul <rosephdarl@gmail.com>
2024-09-24 17:12:50 +0800

    update content

--- react-app/README.md
+++ react-app/README.md
@@ -48,3 +48,4 @@ export default tseslint.config({
   },
 })
 ```
+"# react-app-v1" 
---
+++ react-app/src/Message.tsx
@@ -0,0 +1,9 @@
+// PascalCasing
+function Message() {
+  // JSX: JS + XML
+  const name = "Roseph Dev";
+  if (name) return <h1>{name}!</h1>;
+  return <h1>Hello World!</h1>;
+}
+
+export default Message;
